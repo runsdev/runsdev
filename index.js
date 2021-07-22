8b4130fa2b04f1f4dd8799a1b89d091d854b7dda
runs-15 <harunsixsixfour@gmail.com>
2021-07-22 08:15:49 +0700

    update

--- index.js
+++ index.js
@@ -18,28 +18,6 @@ let DATA = {
   }),
 };
 
-async function setWeatherInformation() {
-  await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
-  )
-    .then(r => r.json())
-    .then(r => {
-      DATA.city_temperature = Math.round(r.main.temp);
-      DATA.city_weather = r.weather[0].description;
-      DATA.city_weather_icon = r.weather[0].icon;
-      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit',
-        timeZone: 'Europe/Stockholm',
-      });
-      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit',
-        timeZone: 'Europe/Stockholm',
-      });
-    });
-}
-
 async function setInstagramPosts() {
   const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('visitstockholm', 3);
   DATA.img1 = instagramImages[0];
@@ -56,11 +34,6 @@ async function generateReadMe() {
 }
 
 async function action() {
-  /**
-   * Fetch Weather
-   */
-  await setWeatherInformation();
-
   /**
    * Get pictures
    */
